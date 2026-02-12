dee873dd556552eed264128e6c64317ae8ac415b
Kenneth Koon <kennethk.dev@gmail.com>
2026-02-12 16:45:29 +1100

    Created a class for product services

---
+++ InventoryAndOrders/Services/ProductServices.cs
@@ -0,0 +1,42 @@
+using Dapper;
+using InventoryAndOrders.Models;
+using InventoryAndOrders.Data;
+using Microsoft.Data.Sqlite;
+
+namespace InventoryAndOrders.Services;
+
+public class ProductServices
+{
+    private readonly Db _db;
+
+    public ProductServices(Db db)
+    {
+        _db = db;
+    }
+
+    // Get a list of all products
+    public IEnumerable<Product> List()
+    {
+        using SqliteConnection conn = _db.CreateConnection();
+        return conn.Query<Product>("SELECT * FROM Products ORDER BY Id");
+    }
+
+    // Add a new product to Db
+    public Product Add(NewProductRequest req)
+    {
+        using SqliteConnection conn = _db.CreateConnection();
+
+        string sql = @"
+            INSERT INTO Products (Name, Price, TotalStock, ReservedStock)
+            VALUES (@Name, @Price, @TotalStock, 0);
+            SELECT last_insert_rowid();
+        ";
+
+        long id = conn.ExecuteScalar<long>(sql, req);
+
+        return conn.QuerySingle<Product>(
+            "SELECT * FROM Products WHERE Id = @Id;",
+            new { Id = id }
+        );
+    }
+}
