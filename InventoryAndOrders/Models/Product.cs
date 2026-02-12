ae4b9ebef6657e95489c2d19c977a2a650508759
Kenneth Koon <kennethk.dev@gmail.com>
2026-02-12 15:23:42 +1100

    Created a Product Model

---
+++ InventoryAndOrders/Models/Product.cs
@@ -0,0 +1,12 @@
+namespace InventoryAndOrders.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public decimal Price { get; set; }
+    public int TotalStock { get; set; }
+    public int ReservedStock { get; set; }
+
+    public int AvailableStock => TotalStock - ReservedStock;
+}
