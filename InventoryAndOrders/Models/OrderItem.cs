f9f35ff5c8505f229a801cf32adedcd24f9776a0
Kenneth Koon <kennethk.dev@gmail.com>
2026-02-15 15:03:02 +1100

    Formatting files

--- InventoryAndOrders/Models/OrderItem.cs
+++ InventoryAndOrders/Models/OrderItem.cs
@@ -1,10 +1,10 @@
 namespace InventoryAndOrders.Models;
 
 public class OrderItem
-{   
+{
     public int Id { get; set; }
     public int OrderId { get; set; }
-    
+
     public int ProductId { get; set; }
     public string ProductName { get; set; } = "";
     public decimal UnitPrice { get; set; }
