668bc559043f4367c24addcc3295cd0a874826a6
Kenneth Koon <kennethk.dev@gmail.com>
2026-02-17 23:50:51 +1100

    Created class for accounts

---
+++ InventoryAndOrders/Models/Account.cs
@@ -0,0 +1,10 @@
+namespace InventoryAndOrders.Models;
+
+public class Account
+{
+    public int Id { get; set; }
+    public string Username { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string PasswordHash { get; set; } = "";
+    public DateTime CreatedAt { get; set; }
+}
