5c32f6dd41bca4aad2aed6565b77c400e4643fba
Kenneth Koon <kennethk.dev@gmail.com>
2026-02-17 15:49:33 +1100

    Created ViewOrderResponse DTOs and modified CreateOrderResponse to return stringed enums

--- InventoryAndOrders/DTOs/OrderResponses.cs
+++ InventoryAndOrders/DTOs/OrderResponses.cs
@@ -6,7 +6,21 @@ public class CreateOrderResponse
 {
     public string OrderNumber { get; set; } = "";
     public string GuestToken { get; set; } = "";
-    public OrderStatus OrderStatus { get; set; }
-    public PaymentStatus PaymentStatus { get; set; }
+    public string OrderStatus { get; set; } = "";
+    public string PaymentStatus { get; set; } = "";
     public decimal TotalPrice { get; set; }
+}
+
+public class ViewOrderResponse
+{
+    public string OrderNumber { get; set; }= "";
+    public string OrderStatus { get; set; } = "";
+    public string PaymentStatus { get; set; } = "";
+    public List<ViewOrderItem> Items { get; set; } = new();
+}
+
+public class ViewOrderItem
+{
+    public string ProductName { get; set; } = "";
+    public int Quantity { get; set; }
 }
