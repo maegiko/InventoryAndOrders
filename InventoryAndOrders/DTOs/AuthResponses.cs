6ff796d3280d818e8a0adb72c89e89e001a79649
Kenneth Koon <kennethk.dev@gmail.com>
2026-02-17 23:50:43 +1100

    Created AuthResponses for auth services

---
+++ InventoryAndOrders/DTOs/AuthResponses.cs
@@ -0,0 +1,9 @@
+namespace InventoryAndOrders.DTOs;
+
+public class RegisterResponse
+{
+    public int Id { get; set; }
+    public string Username { get; set; } = "";
+    public string Email { get; set; } = "";
+    public DateTime CreatedAt { get; set; }
+}
