78b50e18ee90ee3150eaa628dc9ca82f71273873
Kenneth Koon <kennethk.dev@gmail.com>
2026-02-17 21:00:30 +1100

    Created exception classes for cancelling orders

--- InventoryAndOrders/Services/Exceptions/OrderExceptions.cs
+++ InventoryAndOrders/Services/Exceptions/OrderExceptions.cs
@@ -3,7 +3,25 @@ namespace InventoryAndOrders.Services.Exceptions;
 public class InvalidOrderException : Exception
 {
     public InvalidOrderException()
-        : base("invalid Order Credentials.")
+        : base("Invalid Order Credentials.")
+    {
+    }
+}
+
+public class OrderStatusException : Exception
+{
+    public string OrderNumber { get; }
+    public OrderStatusException(string orderNumber)
+        : base($"Order: {orderNumber} is unable to be cancelled.")
+    {
+        OrderNumber = orderNumber;
+    }
+}
+
+public class OrderCancelException : Exception
+{
+    public OrderCancelException()
+        : base("Unable to cancel order due to inventory conflict.")
     {
     }
 }
