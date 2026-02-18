4d1e712f3a33978c3ecbbb73a706b885daf9ae80
Kenneth Koon <kennethk.dev@gmail.com>
2026-02-18 12:54:59 +1100

    Created LoginResponse class

--- InventoryAndOrders/DTOs/AuthResponses.cs
+++ InventoryAndOrders/DTOs/AuthResponses.cs
@@ -7,3 +7,11 @@ public class RegisterResponse
     public string Email { get; set; } = "";
     public DateTime CreatedAt { get; set; }
 }
+
+public class LoginResponse
+{
+    public bool Success { get; set; }
+    public string Message { get; set; } = "";
+    public string Token { get; set; } = "";
+    public DateTime ExpiresAt { get; set; }
+}
