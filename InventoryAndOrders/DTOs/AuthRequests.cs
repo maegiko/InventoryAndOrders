a45fd981f2b7710de1769c017f928ab21644752e
Kenneth Koon <kennethk.dev@gmail.com>
2026-02-18 12:55:11 +1100

    Created LoginRequest class

--- InventoryAndOrders/DTOs/AuthRequests.cs
+++ InventoryAndOrders/DTOs/AuthRequests.cs
@@ -6,3 +6,9 @@ public class RegisterRequest
     public string Email { get; set; } = "";
     public string Password { get; set; } = "";
 }
+
+public class LoginRequest
+{
+    public string Username { get; set; } = "";
+    public string Password { get; set; } = "";
+}
